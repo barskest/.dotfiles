599333ac3198aa22b7da7db58b20f9cd9e0af094
Peter Ostergaard <barskt@gmail.com>
2025-08-11 22:49:44 +0200

    Changing compositor to niri.

--- nixos/system/desktop_environment.nix
+++ nixos/system/desktop_environment.nix
@@ -1,15 +1,27 @@
 { config, pkgs, ... }:
 
+# {
+# 	programs.hyprland.enable = true;
+
+# 	services.displayManager =  {
+# 		defaultSession = "hyprland";
+# 		sddm = {
+# 			enable = true;
+# 			wayland.enable = true;
+# 		};
+# 	};
+
+# 	programs.waybar.enable = true;
+# }
+
 {
-	programs.hyprland.enable = true;
+  # Enable niri compositor
+  programs.niri.enable = true;
 
-	services.displayManager =  {
-		defaultSession = "hyprland";
-		sddm = {
-			enable = true;
-			wayland.enable = true;
-		};
-	};
-
-	programs.waybar.enable = true;
+  # Display manager (SDDM)
+  services.displayManager = {
+    defaultSession = "niri";
+    sddm.enable = true;
+    sddm.wayland.enable = true;
+  };
 }
